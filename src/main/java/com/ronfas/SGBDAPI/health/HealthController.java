7c7c0dcdfd1fac07def861b234680358e897dcd9
Julien Fastre <fastre.j@gmail.com>
2021-02-23 16:36:36 +0100

    Add api prefix and health check

---
+++ src/main/java/com/ronfas/SGBDAPI/health/HealthController.java
@@ -0,0 +1,19 @@
+package com.ronfas.SGBDAPI.health;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    public void getHealth(){
+    }
+
+    @GetMapping("/teapot")
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    public void getTeaPot(){
+    }
+}
