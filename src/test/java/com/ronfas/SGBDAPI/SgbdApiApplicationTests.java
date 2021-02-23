ab0a9c9e3aee2f73ad3b5f50ab69e5f0485b1e6b
Julien Fastre <julien.fastre@cmsj.be>
2021-02-23 14:10:49 +0100

    init spring boot

---
+++ src/test/java/com/ronfas/SGBDAPI/SgbdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronfas.SGBDAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgbdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
