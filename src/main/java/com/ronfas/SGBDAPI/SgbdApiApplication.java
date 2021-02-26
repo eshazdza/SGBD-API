703db99b02c08f66196975dd80d2300f9880171c
Julien Fastre <fastre.j@gmail.com>
2021-02-26 10:12:14 +0100

    Switch from mongodb to mysql

--- src/main/java/com/ronfas/SGBDAPI/SgbdApiApplication.java
+++ src/main/java/com/ronfas/SGBDAPI/SgbdApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SgbdApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SgbdApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SgbdApiApplication.class, args);
+    }
 }
